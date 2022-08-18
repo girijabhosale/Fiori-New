32bd02dbeca23818facc3db0e485fee993eb5636
girija bhosale <girijab.in@mouritech.com>
2022-08-18 05:15:10 +0000

    ui5

--- pichower/pic.js
+++ pichower/pic.js
@@ -5,3 +5,22 @@ function Enlarge(){
 		height: '+=6px'
 	});
 }
+//API call
+function getAllCountries(){
+	$.get("https://api.covid19api.com/countries",function(data)	{
+			var aCountries = data;
+			for (var i=0; i<aCountries.length; i++) {
+				onDraw(aCountries[i].ISO2 + " - " + aCountries[i].Country);
+			}
+	});
+}
+
+function onDraw(countryVal){
+	var oNewElement = document.createElement("h4");
+	//var oText = document.createTextNode(document.getElementById("got").value);
+	var oText = document.createTextNode(countryVal);
+	oNewElement.appendChild(oText);
+	
+	var oCanvas = document.getElementById("canvas");
+	oCanvas.appendChild(oNewElement);
+}
